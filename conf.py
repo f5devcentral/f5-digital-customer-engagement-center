3bb292c5d28e3482e300a52e4af1511bbd0798be
jmcalalang <jon.calalang@gmail.com>
2021-01-13 05:07:34 +0000

    cleaning up the RTD's objects

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
+# html_theme = 'alabaster'
 html_theme_path = ["docs/_themes/"]
 html_theme = 'f5_sphinx_theme'
 
