38d226aa117951f9740637c2fb4ed4207978d2d2
Matthew Emes <m.emes@f5.com>
2021-02-03 11:20:30 -0800

    Chore: cleanup: zone is not used in network min module

--- modules/google/terraform/network/min/variables.tf
+++ modules/google/terraform/network/min/variables.tf
@@ -1,22 +1,10 @@
 # project
-resource "random_pet" "buildSuffix" {
-  keepers = {
-    prefix = var.projectPrefix
-  }
-  length    = 2
-  separator = "-"
-}
-
 variable "projectPrefix" {
   description = "prefix for resources"
   default     = "demo"
 }
 variable "buildSuffix" {
   description = "random build suffix for resources"
-  default     = "random-cat"
-}
-variable "gcpZone" {
-  description = "zone where gke is deployed"
 }
 variable "gcpRegion" {
   description = "region where gke is deployed"
