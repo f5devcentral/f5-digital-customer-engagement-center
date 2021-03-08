e885e481b8437ca316f3fbfde480d2b86cf39a34
vinnie357 <vinnie.mazza@gmail.com>
2021-03-08 15:38:56 +0000

    - templates to folders - export kubeconfig for demo.sh

--- solutions/security/nginx/kic/azure/cleanup.sh
+++ solutions/security/nginx/kic/azure/cleanup.sh
@@ -3,7 +3,8 @@ echo "destroying demo kic-nap aks"
 read -r -p "Are you sure? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-    kubectl delete -f ../templates/
+    kubectl delete -f ../templates/arcadia
+    kubectl delete -f ../templates/kic
     terraform destroy --auto-approve
     rm -f ~/.kube/aks-cluster-config
 else
