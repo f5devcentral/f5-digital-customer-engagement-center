89a7f3ac5d5f0f04093e589d6013aa0226138f50
Eric Chen <eric.chen@f5.com>
2021-04-01 23:53:34 -0400

    user namespace; assisted mode; unique prefix

--- solutions/security/multi-cloud-connectivity-volterra/variables.tf
+++ solutions/security/multi-cloud-connectivity-volterra/variables.tf
@@ -55,3 +55,16 @@ variable "volterraCloudCred" {
   type        = string
   default     = null
 }
+variable "namespace" {
+  description = "Volterra application namespace"
+  type        = string
+  default     = "default"
+}
+variable "assisted" {
+  description = "Use Assisted deployment for Volterra TGW Site"
+  default     = false
+}
+variable "volterraUniquePrefix" {
+  description = "Unique prefix to use for System resources in Volterra tenant"
+  default     = "acme"
+}
