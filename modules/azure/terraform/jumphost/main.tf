61ab3aa8a12b524c54488757c10afeb0adb1304f
jeffgiroux <thejeffgiroux@gmail.com>
2021-04-08 11:36:24 -0700

    ubuntu version 18>20

--- modules/azure/terraform/jumphost/main.tf
+++ modules/azure/terraform/jumphost/main.tf
@@ -83,8 +83,8 @@ resource "azurerm_linux_virtual_machine" "jumphost" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
     version   = "latest"
   }
 
