8188125bdf4e00b99c1a26bd75e9efe8c4967114
jeffgiroux <thejeffgiroux@gmail.com>
2021-04-08 10:46:19 -0700

    variable cleanup

--- modules/azure/terraform/jumphost/variables.tf
+++ modules/azure/terraform/jumphost/variables.tf
@@ -50,14 +50,10 @@ variable "coderAccountPassword" {
 }
 
 # onboarding
-variable "onboardScript" {
-  description = "URL to userdata onboard script"
-  default     = "https://raw.githubusercontent.com/vinnie357/workspace-onboard-bash-templates/master/terraform/aws/sca/onboard.sh"
-}
 variable "repositories" {
   description = "comma seperated list of git repositories to clone"
-  default     = "https://github.com/vinnie357/aws-tf-workspace.git,https://github.com/f5devcentral/terraform-aws-f5-sca.git"
+  default     = null
 }
 variable "terraformVersion" {
-  default = "0.14.0"
+  default = "0.14.10"
 }
