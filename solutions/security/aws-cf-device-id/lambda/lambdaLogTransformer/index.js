44b7618282e3813ffe60019a3302ae9cf151bf51
yossi <yossi.ros@gmail.com>
2021-04-27 18:25:48 +0000

    deviceid logs parsed

--- solutions/security/aws-cf-device-id/lambda/lambdaLogTransformer/index.js
+++ solutions/security/aws-cf-device-id/lambda/lambdaLogTransformer/index.js
@@ -1,25 +1,46 @@
 'use strict'
 exports.handler = (event, context, callback) => {
+
     /* Process the list of records and drop those containing Default_Action */
     const output = event.records.map((record) => {
         
         const entry = (Buffer.from(record.data, 'base64')).toString('utf8');
-        if (!entry.match(/Default_Action/g))
-        {
-            return {
-              
-                recordId: record.recordId,
-                result: 'Ok',
-                data: record.data,
-            };
+        var jsonEntry = JSON.parse(entry);
+        //console.log(jsonEntry.httpRequest.headers)
+        const headers = jsonEntry.httpRequest.headers;
+        let cookie;
+        for (let i =0; i < headers.length; i++) {
+            const item = headers[i];
+            if (item.name == 'cookie') {
+            cookie = item.value;
+            break;
+            }
         }
-        else {
-            return {
-                recordId: record.recordId,
-                result: 'Dropped',
-                data: record.data,
-            };
+        
+        const cookieArray = cookie.split(';');
+        //console.log(cookieArray)
+        for (let j=0; j < cookieArray.length; j++) {
+            const item = cookieArray[j].trim();
+            if (item.indexOf('_imp_di_pc_') == 0) {
+                const name = item.split('=')[0];
+                const value = item.split('=')[1];
+                jsonEntry["deviceidA"] = value;
+            }
+            if (item.indexOf('_imp_apg_r_') == 0) {
+                const name = item.split('=')[0];
+                const value = item.split('=')[1];
+                jsonEntry["deviceidB"] = value;                
+            }
+            //console.log(jsonEntry);
         }
+        let objJsonStr = JSON.stringify(jsonEntry);
+        let objJsonB64 = Buffer.from(objJsonStr).toString("base64");
+        //console.log(jsonEntry)
+        return {
+            recordId: record.recordId,
+            result: 'Ok',
+            data: objJsonB64,
+        };        
     });
   
     console.log(`Processing completed.  Successful records ${output.length}.`);
