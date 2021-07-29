ea7a225f478b4dfeddda0928248f57ebbf9b0c03
jeffgiroux <thejeffgiroux@gmail.com>
2021-07-29 16:47:28 -0700

    versions

--- solutions/security/multi-cloud-connectivity-volterra/azure/versions.tf
+++ solutions/security/multi-cloud-connectivity-volterra/azure/versions.tf
@@ -1,11 +1,11 @@
 # Set minimum Terraform version and Terraform Cloud backend
 terraform {
-  required_version = "~> 0.14"
+  required_version = ">= 0.14.5"
   required_providers {
     volterra = {
       source  = "volterraedge/volterra"
-      version = "0.4.0"
+      version = "0.8.1"
     }
-    azurerm = "~> 2"
+    azurerm = ">= 2.69"
   }
 }
