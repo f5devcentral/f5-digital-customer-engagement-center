cbfcf0cadf56e4b60bc4e365fff725931ebb454c
jeffgiroux <thejeffgiroux@gmail.com>
2021-08-06 11:59:45 -0700

    dns for_each, record now dynamic

--- solutions/security/multi-cloud-connectivity-volterra/azure/dns.tf
+++ solutions/security/multi-cloud-connectivity-volterra/azure/dns.tf
@@ -1,11 +1,12 @@
 ############################ Private DNS Zones ############################
 
 resource "azurerm_private_dns_zone" "sharedAcme" {
+  for_each            = local.vnets
   name                = var.domain_name
-  resource_group_name = azurerm_resource_group.rg["bu11"].name
+  resource_group_name = azurerm_resource_group.rg[each.key].name
 
   tags = {
-    Name      = format("%s-dns-%s", var.resourceOwner, var.buildSuffix)
+    Name      = format("%s-dns-%s-%s", var.resourceOwner, each.key, var.buildSuffix)
     Terraform = "true"
   }
 }
@@ -13,17 +14,19 @@ resource "azurerm_private_dns_zone" "sharedAcme" {
 ############################ Zone Records ############################
 
 resource "azurerm_private_dns_a_record" "inside" {
+  for_each            = local.vnets
   name                = "inside"
-  zone_name           = azurerm_private_dns_zone.sharedAcme.name
-  resource_group_name = azurerm_resource_group.rg["bu11"].name
+  zone_name           = azurerm_private_dns_zone.sharedAcme[each.key].name
+  resource_group_name = azurerm_resource_group.rg[each.key].name
   ttl                 = 300
-  records             = ["10.1.52.6"]
+  records             = [data.azurerm_network_interface.sli[each.key].private_ip_address]
 }
 
 resource "azurerm_private_dns_cname_record" "sharedAcme" {
+  for_each            = local.vnets
   name                = "*"
-  zone_name           = azurerm_private_dns_zone.sharedAcme.name
-  resource_group_name = azurerm_resource_group.rg["bu11"].name
+  zone_name           = azurerm_private_dns_zone.sharedAcme[each.key].name
+  resource_group_name = azurerm_resource_group.rg[each.key].name
   ttl                 = 300
   record              = format("inside.%s", var.domain_name)
 }
@@ -33,7 +36,7 @@ resource "azurerm_private_dns_cname_record" "sharedAcme" {
 resource "azurerm_private_dns_zone_virtual_network_link" "link" {
   for_each              = local.vnets
   name                  = each.key
-  resource_group_name   = azurerm_resource_group.rg["bu11"].name
-  private_dns_zone_name = azurerm_private_dns_zone.sharedAcme.name
+  resource_group_name   = azurerm_resource_group.rg[each.key].name
+  private_dns_zone_name = azurerm_private_dns_zone.sharedAcme[each.key].name
   virtual_network_id    = module.network[each.key].vnet_id
 }
