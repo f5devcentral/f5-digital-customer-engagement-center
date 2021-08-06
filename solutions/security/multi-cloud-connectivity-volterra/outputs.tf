d6f2b3ee361adce220b1fec475ca754e45ecf810
jeffgiroux <thejeffgiroux@gmail.com>
2021-08-05 19:13:29 -0700

    add aws publicIP to outputs

--- solutions/security/multi-cloud-connectivity-volterra/outputs.tf
+++ solutions/security/multi-cloud-connectivity-volterra/outputs.tf
@@ -2,3 +2,8 @@ output "connection_helpers" {
   description = "gcloud connection helpers for GCP workstation(s)"
   value       = local.deploy_google ? module.google[0].connection_helpers : "{}"
 }
+
+output "aws_jumphost_publicIP" {
+  description = "AWS Jumphost Public IP in BU1"
+  value       = local.deploy_aws ? module.aws[0].bu1JumphostPublicIp : "{}"
+}
