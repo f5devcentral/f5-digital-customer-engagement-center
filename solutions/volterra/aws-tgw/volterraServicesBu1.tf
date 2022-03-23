8367b16ffb039e261857d1069708646379176f36
jeffgiroux <thejeffgiroux@gmail.com>
2022-03-22 18:01:21 -0700

    bu1 services

--- solutions/volterra/aws-tgw/volterraServicesBu1.tf
+++ solutions/volterra/aws-tgw/volterraServicesBu1.tf
@@ -1,14 +1,14 @@
-
+############################ HTTP LB and Origin Pool - BU1 ############################
 
 resource "volterra_origin_pool" "bu1app" {
-  name                   = "bu1app"
+  name                   = "${var.projectPrefix}-bu1app-${random_id.buildSuffix.hex}"
   namespace              = var.namespace
   endpoint_selection     = "DISTRIBUTED"
   loadbalancer_algorithm = "LB_OVERRIDE"
+  port                   = 80
+  no_tls                 = true
 
   origin_servers {
-    // One of the arguments from this list "voltadn_private_name srv6_private_name public_ip public_name custom_endpoint_object voltadn_private_ip srv6_private_ip private_ip private_name k8s_service consul_service" must be set
-
     private_ip {
       ip = module.webserver["vpcBu1App1"].workspaceManagementAddress
       site_locator {
@@ -21,22 +21,21 @@ resource "volterra_origin_pool" "bu1app" {
       inside_network = true
     }
 
-    labels = {
-      "bu" = "bu1"
-    }
+    labels = merge(local.volterraCommonLabels, {
+      bu = "bu1"
+    })
   }
-
-  port = 80
-
-  // One of the arguments from this list "no_tls use_tls" must be set
-  no_tls = true
 }
 
 resource "volterra_http_loadbalancer" "bu1app1" {
-  name      = "bu1app1"
-  namespace = var.namespace
-
-  // One of the arguments from this list "do_not_advertise advertise_on_public_default_vip advertise_on_public advertise_custom" must be set
+  name                            = "${var.projectPrefix}-bu1app1-${random_id.buildSuffix.hex}"
+  namespace                       = var.namespace
+  no_challenge                    = true
+  domains                         = ["bu1app.shared.acme.com"]
+  random                          = true
+  disable_rate_limit              = true
+  service_policies_from_namespace = true
+  disable_waf                     = true
 
   advertise_custom {
     advertise_where {
@@ -81,39 +80,8 @@ resource "volterra_http_loadbalancer" "bu1app1" {
       name = volterra_origin_pool.bu1app.name
     }
   }
-  // One of the arguments from this list "no_challenge js_challenge captcha_challenge policy_based_challenge" must be set
-  no_challenge = true
-
-  domains = ["bu1app.shared.acme.com"]
-
-  // One of the arguments from this list "least_active random source_ip_stickiness cookie_stickiness ring_hash round_robin" must be set
-  random = true
-
-  // One of the arguments from this list "http https_auto_cert https" must be set
 
   http {
     dns_volterra_managed = false
   }
-
-  // One of the arguments from this list "disable_rate_limit rate_limit" must be set
-  disable_rate_limit = true
-  #  rate_limit {
-  #    // One of the arguments from this list "custom_ip_allowed_list no_ip_allowed_list ip_allowed_list" must be set
-  #
-  #    ip_allowed_list {
-  #      prefixes = ["0.0.0.0/0"]
-  #    }
-  #
-  #    // One of the arguments from this list "no_policies policies" must be set
-  #    no_policies = true
-  #
-  #    rate_limiter {
-  #      total_number = "total_number"
-  #      unit         = "unit"
-  #    }
-  #  }
-  // One of the arguments from this list "service_policies_from_namespace no_service_policies active_service_policies" must be set
-  service_policies_from_namespace = true
-  // One of the arguments from this list "disable_waf waf waf_rule" must be set
-  disable_waf = true
 }
