f10a719652e5393148a1f0064d0e1a99d6448fb8
jeffgiroux <thejeffgiroux@gmail.com>
2022-03-22 17:08:51 -0700

    outputs

--- solutions/volterra/aws-tgw/outputs.tf
+++ solutions/volterra/aws-tgw/outputs.tf
@@ -28,24 +28,3 @@ output "vpcIdBu2" {
   description = "vpc id"
   value       = module.vpcBu2.vpc_id
 }
-
-#output "vpcIdTransitAcme" {
-#  description = "vpc id"
-#  value       = module.vpcTransitAcme.vpc_id
-#}
-#output "vpcIdTransitBu1" {
-#  description = "vpc id"
-#  value       = module.vpcTransitBu1.vpc_id
-#}
-#output "vpcIdTransitBu2" {
-#  description = "vpc id"
-#  value       = module.vpcTransitBu2.vpc_id
-#}
-#output "bigipPublicIp" {
-#  description = "Public ip for the BIGIP, access on port 8443"
-#  value       = module.gwlb-bigip.bigipAz1Ip
-#}
-#output "bigipPassword" {
-#  description = "Password for the admin usernmae"
-#  value       = module.gwlb-bigip.bigipPassword
-#}
