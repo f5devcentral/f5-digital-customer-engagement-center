83d752299d9a20998f7a67cdbd8f94e6d78bb176
JeffGiroux <thejeffgiroux@gmail.com>
2022-10-10 19:03:21 -0700

    vmss monitoring policy

--- solutions/delivery/application_delivery_controller/nginx/nginx-for-azure/appservers.tf
+++ solutions/delivery/application_delivery_controller/nginx/nginx-for-azure/appservers.tf
@@ -147,7 +147,64 @@ resource "azurerm_monitor_autoscale_setting" "appWest" {
   }
   notification {
     webhook {
-      service_uri = "https://test"
+      service_uri = "https://${azurerm_windows_function_app.main.default_hostname}/api/vmAutoscaleNginxConfig?code=${data.azurerm_function_app_host_keys.main.default_function_key}"
+    }
+  }
+}
+
+# Create Autoscale settings for East region app servers
+resource "azurerm_monitor_autoscale_setting" "appEast" {
+  name                = "autoscale"
+  resource_group_name = azurerm_resource_group.appEast.name
+  location            = azurerm_resource_group.appEast.location
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.appEast.id
+  profile {
+    name = "default"
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 3
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.appEast.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 75
+      }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.appEast.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 25
+      }
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+  notification {
+    webhook {
+      service_uri = "https://${azurerm_windows_function_app.main.default_hostname}/api/vmAutoscaleNginxConfig?code=${data.azurerm_function_app_host_keys.main.default_function_key}"
     }
   }
 }
